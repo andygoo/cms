2808f841216e7af6b40662d268e12325c22273b3
andygoo <i@jiesc.net>
2016-01-26 20:20:55 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- views/home.php
+++ views/home.php
@@ -4,13 +4,15 @@
     <li class="media post">
         <div class="media-left" style="padding-right: 15px">
             <?php if (strpos($item['pic'], '://') !== false):?>
-            <?= HTML::image($item['pic'].'?imageView2/2/w/160/h/120', array('width'=>160)) ?>
+            <?= HTML::image($item['pic'].'?imageView2/2/w/400/h/300', array('width'=>200)) ?>
             <?php else:?>
-            <?= HTML::image('/imagefly/w200-h150-c/' . $item['pic'], array('width'=>160)) ?>
+            <?= HTML::image('/imagefly/w400-h300-c/' . $item['pic'], array('width'=>200)) ?>
             <?php endif;?>
         </div>
         <div class="media-body post-head">
-            <h4 class="media-heading post-title" style="font-size: 26px;"><a href="<?= URL::site('article?id='.$item['id'])?>"><?= $item['title'] ?></a></h4>
+            <h1 class="media-heading post-title" style="font-size: 26px;">
+                <a href="<?= URL::site('article?id='.$item['id'])?>"><?= $item['title'] ?></a>
+            </h1>
             <div class="post-content" style="margin: 10px 0;"><?= Text::limit_chars($item['brief'],80) ?></div>
         </div>
         <footer class="post-footer clearfix">
