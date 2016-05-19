a5b095ade300791729c7962758e3667456ed7d94
andygoo <i@jiesc.net>
2016-05-19 17:40:35 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- mobile/common/footer.php
+++ mobile/common/footer.php
@@ -1,8 +1,4 @@
-
 <script>
-var _maq = _maq || [];
-_maq.push(['_setAccount', '222222']);
-_maq.push(['_trackPageview']);
 (function() {
     var ma = document.createElement('script'); ma.type = 'text/javascript'; ma.async = true;
     ma.src = document.location.protocol + '//127.0.0.1:10099/ma.js';
