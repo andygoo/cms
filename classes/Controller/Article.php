7d1b72d07230ec584c4e410062f5df1ceffa5b43
andygoo <i@jiesc.net>
2016-06-14 17:15:43 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- classes/Controller/Article.php
+++ classes/Controller/Article.php
@@ -49,6 +49,33 @@ class Controller_Article extends Controller_Website {
         $this->content->next_page = $pagers;
     }
     
+
+    public function action_lists() {
+        $cid = Arr::get($_GET, 'cid');
+        
+        $where = array();
+        $where['cid'] = $cid;
+        $where['status'] = 'open';
+        $where['ORDER'] = 'id DESC';
+        $where = array_filter($where);
+        
+        $m_article = Model::factory('article', 'cms');
+        $total = $m_article->count($where);
+        $pager = new Pager($total, 10);
+        $list = $m_article->select($pager->offset, $pager->size, $where)->as_array();
+        
+        $m_category = Model::factory('category', 'cms');
+        $cat_list = $m_category->getAll()->as_array('id', 'name');
+        foreach ($list as &$item) {
+            $item['cat_name'] = isset($cat_list[$item['cid']]) ? $cat_list[$item['cid']] : '';
+        }
+        unset($item);
+        
+        $this->content = View::factory('article_list');
+        $this->content->list = $list;
+        $this->content->pager = $pager;
+    }
+    
     public function action_detail() {
         $id = Arr::get($_GET, 'id');
         $m_article = Model::factory('article', 'cms');
---
+++ views/article_list.php
@@ -0,0 +1,31 @@
+
+<ul class="media-list">
+<?php foreach($list as $item): ?>
+    <li class="media post">
+        <div class="media-left" style="padding-right: 15px">
+            <?php if (strpos($item['pic'], '://') !== false):?>
+            <?= HTML::image($item['pic'].'?imageView2/2/w/400/h/300', array('width'=>200)) ?>
+            <?php else:?>
+            <?= HTML::image('/imagefly/w400-h300-c/' . $item['pic'], array('width'=>200)) ?>
+            <?php endif;?>
+        </div>
+        <div class="media-body post-head">
+            <h1 class="media-heading post-title" style="font-size: 26px;">
+                <a href="<?= URL::site('article?id='.$item['id'])?>"><?= $item['title'] ?></a>
+            </h1>
+            <div class="post-content" style="margin: 10px 0;"><?= Text::limit_chars($item['brief'],80) ?></div>
+        </div>
+        <footer class="post-footer clearfix">
+            <div class="pull-left tag-list">
+                <i class="fa fa-folder-open-o"></i>
+                <a href="<?= URL::site('?cid='.$item['cid'])?>"><?= $item['cat_name']?></a>
+            </div>
+            <div class="pull-right tag-list">
+                <time class="post-date"><?= date('Y-m-d', $item['add_time'])?></time>
+            </div>
+        </footer>
+    </li>
+<?php endforeach; ?>
+</ul>
+
+<?= $pager?>
