4d8e0ae39c58670931c17b82ed08133f253e6f31
andygoo <i@jiesc.net>
2016-06-29 12:30:26 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- views/article_index.php
+++ views/article_index.php
@@ -20,15 +20,15 @@
 <div class="content-section">
 <div class="container">
 <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-4 col-sm-4">
         <h2 class="images">0</h2>
         <h3>Images</h3>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4 col-sm-4">
         <h2 class="imageCounter">0</h2>
         <h3>Images Served</h3>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4 col-sm-4">
         <h2 class="bandwidthCounter">0 GB</h2>
         <h3>Bandwidth Used</h3>
     </div>
@@ -36,16 +36,16 @@
 </div>
 </div>
 
+<?php foreach (range(1, 5) as $idx):?>
+<?php if ($idx % 2):?>
 <div class="content-section-right">
 <div class="container">
 <div class="row">
     <div class="col-lg-5 col-sm-6">
         <div class="clearfix"></div>
-        <h2 class="section-heading">Basic Usage</h2>
-        <p class="lead">Just put your image size (width &amp; height) after our URL and you'll get a placeholder.</p>
-        <pre><code>https://unsplash.it/200/300</code></pre>
-        <p class="lead">To get a square image, just put the size you want.</p>
-        <pre><code>https://unsplash.it/200</code></pre>
+        <h2 class="section-heading">Random image</h2>
+        <p class="lead">Get a random image by appending <code>?random</code> to the end of the url.</p>
+        <p class="lead">Get a random image by appending <code>?random</code> to the end of the url.</p>
     </div>
     <div class="col-lg-5 col-lg-offset-2 col-sm-6">
         <img class="img-responsive" src="https://unsplash.it/458/354" alt="">
@@ -53,72 +53,24 @@
 </div>
 </div>
 </div>
-
-<div class="content-section-left">
-<div class="container">
-    <div class="row">
-        <div class="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6">
-            <div class="clearfix"></div>
-            <h2 class="section-heading">Random image</h2>
-            <p class="lead">Get a random image by appending <code>?random</code> to the end of the url.</p>
-            <pre><code>https://unsplash.it/200/300/?random</code></pre>
-        </div>
-        <div class="col-lg-5 col-sm-pull-6 col-sm-6">
-            <img class="img-responsive random-image" src="https://unsplash.it/458/354?random" alt="">
-        </div>
-    </div>
-</div>
-</div>
-
-<div class="content-section-right">
-<div class="container">
-<div class="row">
-    <div class="col-lg-5 col-sm-6">
-        <div class="clearfix"></div>
-        <h2 class="section-heading">Specific Image</h2>
-        <p class="lead">Get a specific image by appending <code>?image</code> to the end of the url.</p>
-        <pre><code>https://unsplash.it/200/300?image=0</code></pre>
-        <p>You can find a list of all images <a href="https://unsplash.it/images">here.</a></p>
-    </div>
-    <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-        <img class="img-responsive" src="https://unsplash.it/458/354?image=0" alt="">
-    </div>
-</div>
-</div>
-</div>
-
+<?php else:?>
 <div class="content-section-left">
 <div class="container">
 <div class="row">
     <div class="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6">
         <div class="clearfix"></div>
-        <h2 class="section-heading">Blurred image</h2>
-        <p class="lead">Get a blurred image by appending <code>?blur</code> to the end of the url.</p>
-        <pre><code>https://unsplash.it/200/300/?blur</code></pre>
+        <h2 class="section-heading">Random image</h2>
+        <p class="lead">Get a random image by appending <code>?random</code> to the end of the url.</p>
+        <p class="lead">Get a random image by appending <code>?random</code> to the end of the url.</p>
     </div>
     <div class="col-lg-5 col-sm-pull-6 col-sm-6">
-        <img class="img-responsive" src="https://unsplash.it/458/354?image=0&blur" alt="">
+        <img class="img-responsive random-image" src="https://unsplash.it/458/354?random" alt="">
     </div>
 </div>
 </div>
 </div>
-
-<div class="content-section-right">
-<div class="container">
-<div class="row">
-    <div class="col-lg-5 col-sm-6">
-        <div class="clearfix"></div>
-        <h2 class="section-heading">Crop Gravity</h2>
-        <p class="lead">Select the cropping gravity by adding <code>?gravity</code> to the end of the url.</p>
-        <p>Valid options are: <code>north, east, south, west, center</code></p>
-        <pre><code>https://unsplash.it/200/300/?gravity=east</code></pre>
-    </div>
-    <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-        <img class="img-responsive" src="https://unsplash.it/458/354?gravity=east" alt="">
-    </div>
-</div>
-</div>
-</div>
+<?php endif;?>
+<?php endforeach;?>
 
 <footer style="background: url(http://7xkkhh.com1.z0.glb.clouddn.com/2016/06/28/14671191571578.jpg)">
 <div class="footer-overlay">
