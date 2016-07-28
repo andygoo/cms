44f8269c0ad008c29cd9598b23fdb26e149c16e8
andygoo <i@jiesc.net>
2016-07-28 16:22:03 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- classes/Controller/Member.php
+++ classes/Controller/Member.php
@@ -10,5 +10,9 @@ class Controller_Member extends Controller_Website {
         }
     }
     
+    public function mypai() {
+        $m_bidlog = Model::factory('bidlog');
+        $list = $m_bidlog->getAll(array('user_id'=>$this->user['id']));
+    }
 }
 
