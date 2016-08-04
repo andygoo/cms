df07ad5314d7d1fd5d588b35e983c2e2fbcdf5c7
andygoo <i@jiesc.net>
2016-08-04 12:36:32 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- mobile/material_template.php
+++ mobile/material_template.php
@@ -4,7 +4,17 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-<meta name="theme-color" content="#00bcd4">
+<?php 
+$theme_arr = array(
+    'red-blue' => '#f44336',
+    'blue-red' => '#2196f3',
+    'teal-red' => '#009688',
+    'cyan-red' => '#00bcd4',
+    'deep_orange-blue' => '#ff5722',
+);
+$theme = 'cyan-red';
+?>
+<meta name="theme-color" content="<?php echo $theme_arr[$theme]?>">
 <link rel="shortcut icon" href="http://ns13.bdstatic.com/static/news/webapp/webappandroid/img/webapp-news-logo.png">
 <!-- Add to homescreen for Chrome on Android -->
 <meta name="mobile-web-app-capable" content="yes">
@@ -16,7 +26,7 @@
 <link rel="apple-touch-icon-precomposed" href="/media/img/apple-touch-icon.png">
 <title>Material</title>
 <?= HTML::style('media/MDicons/css/MDicon.min.css')?>
-<?= HTML::style('media/mdl/material.cyan-red.min.css')?>
+<?= HTML::style('media/mdl/material.'.$theme.'.min.css')?>
 <style>
 .mdl-layout-title {color: #fff}
 header .mdicon{font-size: 24px;color: #fff}
