e6d9650696b0994ac69279c5201a3dfb44911fd2
andygoo <i@jiesc.net>
2016-08-10 11:58:29 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- mobile/material_feedback.php
+++ mobile/material_feedback.php
@@ -18,65 +18,97 @@
 </header>
 
 <?= HTML::style('media/css/chat.css')?>
-<main class="mdl-layout__content" style="padding-bottom:65px">
+<main class="mdl-layout__content" id="content" style="padding-bottom:65px">
 
 <style>
-.chat-thread li:nth-child(even) .user {
+.chat-thread li.left .user {
     position: absolute;left: -74px;top:4px;color:#fff;
 }
-.chat-thread li:nth-child(odd) .user {
+.chat-thread li.right .user {
     position: absolute;right: -74px;top:4px;color:#fff;
 }
-.chat-thread li:nth-child(odd):before, 
-.chat-thread li:nth-child(even):before {
+.chat-thread li.right:before, 
+.chat-thread li.left:before {
     background: <?= $theme_color?>;
 }
 /*
-.chat-thread li:nth-child(even) {
+.chat-thread li.left {
 	background-color: #fff;
 }
-.chat-thread li:nth-child(even):after {
+.chat-thread li.left:after {
 	border-top: 15px solid #fff;
 }*/
 .send-msg-form {
-    position: fixed;left:0;bottom:0;z-index:9;
-    background:#fff;width:100%;padding: 0 6px;
-    box-shadow: 0 -2px 5px 0 rgba(0, 0, 0, 0.1), 0 -2px 10px 0 rgba(0, 0, 0, 0.05);
+    position: fixed; left:0; bottom:0; width:100%; padding: 0 6px; margin:0; z-index:9;
+    background:#fff; box-shadow: 0 -2px 5px 0 rgba(0, 0, 0, 0.1), 0 -2px 10px 0 rgba(0, 0, 0, 0.05);
 }
 </style>
-<ul class="chat-thread">
-	<li><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
-	<li><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
-	<li><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
-  	<li><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
-	<li><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
-	<li><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
-  	<li><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
-	<li><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
-	<li><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
-  	<li><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
-	<li><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
-	<li><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
+<ul class="chat-thread" id="chat-thread">
+	<li class="right"><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
+	<li class="left"><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
+	<li class="right"><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
+  	<li class="left"><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
+	<li class="right"><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
+	<li class="left"><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
+  	<li class="right"><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
+	<li class="left"><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
+	<li class="right"><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
+  	<li class="left"><i class="mdicon user mdicon-sm"></i>Are we meeting today?</li>
+	<li class="right"><i class="mdicon user mdicon-sm"></i>yes, what time suits you?</li>
+	<li class="left"><i class="mdicon user mdicon-sm"></i>I was thinking after lunch, I have a meeting in the morning</li>
 </ul>
 
-<form action="#" class="send-msg-form">
-
-    <ul class="mdl-list" style="margin: 0;padding: 0">
-      <li class="mdl-list__item" style="padding: 0 20px 0 5px;">
-          <span class="mdl-list__item-primary-content">
-              <div class="mdl-textfield mdl-js-textfield" style="width: 96%;">
-                <textarea class="mdl-textfield__input" rows="1" id="sample5" style="padding: 2px 0;"></textarea>
-                <label class="mdl-textfield__label" for="sample5">请输入...</label>
-              </div>
-          </span>
-          <a class="mdl-list__item-secondary-action" href="#">
-            <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
-                <i class="mdicon send mdicon-xs"></i>
-            </button>
-          </a>
-      </li>
-    </ul>
-</form>
+<ul class="mdl-list send-msg-form">
+  <li class="mdl-list__item" style="padding: 0 20px 0 5px;">
+      <span class="mdl-list__item-primary-content">
+          <div class="mdl-textfield mdl-js-textfield" style="width: 96%;">
+            <textarea class="mdl-textfield__input" rows="1" id="msg_input" style="padding: 2px 0;"></textarea>
+            <label class="mdl-textfield__label" for="msg_input">请输入...</label>
+          </div>
+      </span>
+      <a class="mdl-list__item-secondary-action" id="send_msg_btn" href="#">
+        <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
+            <i class="mdicon send mdicon-xs"></i>
+        </button>
+      </a>
+  </li>
+</ul>
 
 </main>
 </div>
+
+<script>
+//var ws = new WebSocket("ws://127.0.0.1:10089/");
+var ws = new WebSocket("ws://192.168.50.178:10089/");
+ws.onopen = function() {
+};
+ws.onclose = function() {
+};
+ws.onmessage = function(evt) {
+	var data = JSON.parse(event.data);
+	console.log(data);
+	$('#chat-thread').append('<li class="left"><i class="mdicon user mdicon-sm"></i>'+data.msg+'</li>');
+	$('#content')[0].scrollTop = $('#content')[0].scrollHeight;
+};
+function sendmsg(msg) {
+	var data = {
+		'type': 'msg',
+		'msg': msg,
+	};
+	ws.send(JSON.stringify(data));
+	
+	$('#chat-thread').append('<li class="right"><i class="mdicon user mdicon-sm"></i>'+data.msg+'</li>');
+	$('#content')[0].scrollTop = $('#content')[0].scrollHeight;
+	$('#msg_input').val('').focus();
+}
+$(function() {
+	$('#send_msg_btn').click(function() {
+		var msg = $('#msg_input').val();
+		if ($.trim(msg) == '') {
+			return false;
+		}
+		sendmsg(msg);
+		return false;
+	});
+});
+</script>
